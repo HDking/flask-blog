4c3900146e778897bfb6421aacc41644a925d53b
Hao Dinh <h.c.dinh1@gmail.com>
2016-04-22 10:40:23 +0200

    nice stuff

---
+++ sql.py
@@ -0,0 +1,17 @@
+# sql.py - Create a SQLite3 table and populate it with data
+
+import sqlite3
+
+#create a new database if the database doesn't already exist
+with sqlite3.connect("blog.db") as connection:
+
+	#get a cusor object used to execute SQL commands
+	c = connection.cursor()
+
+	#create the table
+	c.execute("""CREATE TABLE posts (title TEXT, post TEXT) """)
+
+	c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
+	c.execute('INSERT INTO posts VALUES("well", "I\'m well.")')
+	c.execute('INSERT INTO posts VALUES("Excellent", "I\'m Excellent.")')
+	c.execute('INSERT INTO posts VALUES("Okay", "I\'m okay.")')
